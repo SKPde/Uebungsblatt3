bed3407458b979c02829c95b39dc2dcb667f06a0
s.kuebel <s.kuebel@otterbach.de>
2017-08-03 16:03:46 +0200

    Etwas getestet

--- src/Hauptprogramm5.java
+++ src/Hauptprogramm5.java
@@ -3,7 +3,11 @@ public class Hauptprogramm5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ListeL<String> test = new ListeL<String>();
+		test.insert("Halberstung");
+		test.insert("DJ Zoller");
 		
+		System.out.println(test);
 	}
 
 }
